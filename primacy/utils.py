5b3be2ad8bdcf479ebc8b229fba6d6106dfcfee9
Tara <tfursten@asu.edu>
2019-03-13 14:03:22 -0700

    rm dask

--- primacy/utils.py
+++ primacy/utils.py
@@ -1,7 +1,7 @@
 import json
-from dask import dataframe as dd
-from dask.multiprocessing import get
-
+# from dask import dataframe as dd
+# from dask.multiprocessing import get
+ 
 
 def read_config(config):
     with open(config, 'r') as json_in:
@@ -14,15 +14,15 @@ def write_json_obj(json_obj, json_path):
     with open(json_path, 'w') as json_out:
         json.dump(json_obj, json_out)
 
-def parallel_apply(df, func, column_map, threads, meta):
-    return dd.from_pandas(
-        df, npartitions=threads).\
-            map_partitions(
-            lambda df : df.apply(
-         lambda x : func(**{
-             k: x[v] for k, v in column_map.items()}), axis=1),
-             meta=meta).\
-            compute(scheduler='threads')
+# def parallel_apply(df, func, column_map, threads, meta):
+#     return dd.from_pandas(
+#         df, npartitions=threads).\
+#             map_partitions(
+#             lambda df : df.apply(
+#          lambda x : func(**{
+#              k: x[v] for k, v in column_map.items()}), axis=1),
+#              meta=meta).\
+#             compute(scheduler='threads')
 
 
 
